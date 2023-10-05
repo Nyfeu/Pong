210bc994b25aea54acca7899074feb93b52d264f
Nyfeu <andre.maiolini@hotmail.com.br>
2023-10-05 16:28:42 -0300

    Criando a janela do jogo

--- main.cpp
+++ main.cpp
@@ -7,5 +7,30 @@ int main() {
 
     cout << "Starting the game..." << endl;
 
+    // Defining the window dimensions
+
+	const int screen_width = 1200;
+	const int screen_height = 800;
+	
+	// Initializing the window
+
+	InitWindow(screen_width, screen_height, "My Pong Game!");
+
+    // Defining Frame Rate
+
+    SetTargetFPS(60);
+
+    // Game Loop
+
+	while ( WindowShouldClose() == false ) {
+        BeginDrawing();
+
+        EndDrawing();
+    }
+
+	// Closing the window
+
+	CloseWindow();
+
     return 0;
 }
